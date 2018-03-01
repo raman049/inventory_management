16f044f8554341e8bc4e4ecf474fa8fe7de439ee
raman049 <raman049@yahoo.com>
2018-03-01 01:07:41 -0600

    Add files via upload

--- Inventory-Management/src/main/java/com/cubics/im/entity/product/ProductEntity.java
+++ Inventory-Management/src/main/java/com/cubics/im/entity/product/ProductEntity.java
@@ -1,15 +1,23 @@
 package com.cubics.im.entity.product;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.SequenceGenerator;
@@ -18,6 +26,7 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 import com.cubics.im.entity.vendor.QueryConstant;
+import com.cubics.im.entity.vendor.VendorEntity;
 
 @Entity
 @Table(name = "PRODUCT")
@@ -47,27 +56,42 @@ public class ProductEntity {
 	@Enumerated(EnumType.STRING)
 	@Column(name = "STATUS")
 	private ProductStatus status;
-
 	@Column(name = "CREATE_DATE")
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date createdDate;// = new Date();
 	@Temporal(TemporalType.TIMESTAMP)
 	@Column(name = "MODIFY_DATE")
 	private Date modifyDate;
-	@Column(name = "PRIMARY_VENDOR")
-	private String primaryVendor;
-	@Column(name = "SECONDARY_VENDOR")
-	private String secondaryVendor;
+	//@OneToMany(fetch=FetchType.LAZY)
+	//@JoinColumn(name = "PRIMARY_VENDOR")
+	//@Column(name="PRIMARY_VENDOR")
+	//private String primaryVendor;
+//	@Column(name = "SECONDARY_VENDOR")
+//	private String secondaryVendor;
 
-	//@OneToMany(targetEntity=VendorEntity.class, fetch=FetchType.LAZY,mappedBy="productEntity")
-//	 List<VendorEntity> vendorEntity = new ArrayList<VendorEntity>();
-//	public List<VendorEntity> getVendorEntity() {
-//		return vendorEntity;
-//	}
-//
-//	public void setVendorEntity(List<VendorEntity> vendorEntity) {
-//		this.vendorEntity = vendorEntity;
-//	}
+	@ManyToOne(fetch = FetchType.LAZY, cascade= CascadeType.PERSIST)
+	@JoinColumn(name="PRIMARY_VENDOR")
+	private VendorEntity primaryVendor;
+
+	@ManyToOne(fetch = FetchType.LAZY, cascade= CascadeType.PERSIST)
+	@JoinColumn(name="SECONDARY_VENDOR")
+	private VendorEntity secondaryVendor;
+
+	public VendorEntity getPrimaryVendor() {
+		return primaryVendor;
+	}
+
+	public void setPrimaryVendor(VendorEntity primaryVendor) {
+		this.primaryVendor = primaryVendor;
+	}
+
+	public VendorEntity getSecondaryVendor() {
+		return secondaryVendor;
+	}
+
+	public void setSecondaryVendor(VendorEntity secondaryVendor) {
+		this.secondaryVendor = secondaryVendor;
+	}
 
 	public long getPk() {
 		return pk;
@@ -149,29 +173,29 @@ public class ProductEntity {
 		this.modifyDate = modifyDate;
 	}
 
-	public String getPrimaryVendor() {
-		return primaryVendor;
-	}
+//	public String getPrimaryVendor() {
+//		return primaryVendor;
+//	}
+//
+//	public void setPrimaryVendor(String primaryVendor) {
+//		this.primaryVendor = primaryVendor;
+//	}
 
-	public void setPrimaryVendor(String primaryVendor) {
-		this.primaryVendor = primaryVendor;
-	}
-
-	public String getSecondaryVendor() {
-		return secondaryVendor;
-	}
-
-	public void setSecondaryVendor(String secondaryVendor) {
-		this.secondaryVendor = secondaryVendor;
-	}
+//	public String getSecondaryVendor() {
+//		return secondaryVendor;
+//	}
+//
+//	public void setSecondaryVendor(String secondaryVendor) {
+//		this.secondaryVendor = secondaryVendor;
+//	}
 
 	@Override
 	public String toString() {
 		return "ProductEntity [pk=" + pk + ", productName=" + productName + ", description=" + description + ", code="
 				+ code + ", currentQuantity=" + currentQuantity + ", orderQuantity=" + orderQuantity
 				+ ", threshodQuantity=" + thresholdQuantity + ", satatus=" + status + ", createdDate=" + createdDate
-				+ ", modifyDate=" + modifyDate + ", primaryVendor=" + primaryVendor + ", secondaryVendor="
-				+ secondaryVendor + "]";
+				+ ", modifyDate=" + modifyDate + ", primaryVendor="  + ", secondaryVendor="
+				+  "]";
 	}
 
 	@PrePersist
--- Inventory-Management/src/test/java/ProductVendorRelationTest.java
+++ Inventory-Management/src/test/java/ProductVendorRelationTest.java
@@ -1,24 +1,29 @@
+import java.util.Date;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
-import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
-
-import org.springframework.stereotype.Service;
+import javax.persistence.Persistence;
 
 import com.cubics.im.entity.product.ProductEntity;
 import com.cubics.im.entity.product.ProductStatus;
 import com.cubics.im.entity.vendor.VendorEntity;
 import com.cubics.im.entity.vendor.VendorStatus;
+import com.cubics.im.service.vendor.VendorVO;
 
-@Service
-@Transactional
 public class ProductVendorRelationTest {
 
-	@PersistenceContext
-	private static EntityManager em;
 	public static void main(String[] args) {
+		EntityManagerFactory emf = null;
+		EntityManager em = null;
+		EntityTransaction et = null;
+
 		try {
+			emf = Persistence.createEntityManagerFactory("OracleUnit");
+			em = emf.createEntityManager();
+			System.out.println("connected to database");
+			et = em.getTransaction();
+			et.begin();
 			VendorEntity ve = new VendorEntity();
 			ve.setAddress1("address1");
 			ve.setAddress2("address2");
@@ -28,52 +33,62 @@ public class ProductVendorRelationTest {
 			ve.setPhone("91919911");
 			ve.setState("Texas");
 			ve.setStatus(VendorStatus.active);
-			ve.setVendorCode("IOP9");
-			ve.setVendorName("vendorName");
+			ve.setVendorCode("IO99");
+			ve.setVendorName("vendrName");
 			ve.setZip("949490");
-			ve.setId(new Long(21));
-
+			
 			VendorEntity ve2 = new VendorEntity();
-			ve2.setAddress1("2222");
-			ve2.setAddress2("22");
-			ve2.setCity("22");
+			ve2.setAddress1("address21");
+			ve2.setAddress2("address22");
+			ve2.setCity("Dallas2");
 			ve2.setEmail("email@email.com");
-			ve2.setFax("123932293");
+			ve2.setFax("1239393");
 			ve2.setPhone("91919911");
 			ve2.setState("Texas");
 			ve2.setStatus(VendorStatus.active);
-			ve2.setVendorCode("POP9");
-			ve2.setVendorName("vendorName");
+			ve2.setVendorCode("2O99");
+			ve2.setVendorName("vendr2Name");
 			ve2.setZip("949490");
-			ve2.setId(new Long(21));
-		
-			ProductEntity pe = new ProductEntity();
-			pe.setCode("EOEO");
-			pe.setCurrentQuantity("90");
-			pe.setDescription("sd");
-			pe.setOrderQuantity("90");
-		//	pe.getVendorEntity().add(ve);
-			//pe.getVendorEntity().add(ve2);
-		//	pe.setPrimaryVendor(primaryVendor);
-			pe.setProductName("name");
-		//	pe.setSecondaryVendor(secondaryVendor);
-			pe.setStatus(ProductStatus.active);
-			 System.out.println(pe);
-			//em.persist(pe);
-			em.persist(ve);
-			
 
-			// et.commit();
-
+//			ProductEntity pe = new ProductEntity();
+//			pe.setCode("11OP");
+//			pe.setCurrentQuantity("1010");
+//			pe.setDescription("k1111kl");
+//			pe.setOrderQuantity("11010");
+//			pe.setProductName("111111");
+//			pe.setStatus(ProductStatus.active);
+//			pe.setThresholdQuantity("1100");
+//			pe.setModifyDate(new Date());
+//			pe.setCreatedDate(new Date());
+//			ve2.getProductEntity().add(pe);
+//			ve.getProductEntity().add(pe);
+//			pe.setPrimaryVendor(ve);
+//			pe.setSecondaryVendor(ve2);
+			//em.persist(ve);
+			//em.persist(ve2);
+		//	em.persist(pe);
+			 ProductEntity pe2 = new ProductEntity();
+			 pe2.setCode("OP2P");
+			 pe2.setCurrentQuantity("100");
+			 pe2.setDescription("kkl");
+			 pe2.setOrderQuantity("1010");
+			 pe2.setProductName("name");
+			 pe2.setStatus(ProductStatus.active);
+			 pe2.setThresholdQuantity("100");
+			 pe2.setModifyDate(new Date());
+			 pe2.setCreatedDate(new Date());
+			// ve2.getProductEntity().add(pe2);
+			 //ve.getProductEntity().add(pe2);
+			 pe2.setPrimaryVendor(em.find(VendorEntity.class, new Long(1070)));
+			 pe2.setSecondaryVendor(em.find(VendorEntity.class, new Long(951)));
+			 em.persist(pe2);
+			// em.persist(ve2);
+			// em.persist(ve);
+			et.commit();
 		} catch (Exception e) {
 			e.printStackTrace();
-			// et.setRollbackOnly();
-
+			et.setRollbackOnly();
 		} finally {
-			if (em != null)
-				em.close();
-//			if (emf != null)
-//				emf.close();
 
 		}
 	}
